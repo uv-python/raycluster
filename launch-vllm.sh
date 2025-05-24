3cfcb4049d146eb97c31dabc0e967a2d016bcb10
Ugo Varetto <ugovaretto@gmail.com>
2025-05-24 15:41:50 +0800

    Added num nodes parameter to slurm scripts

--- launch-vllm.sh
+++ launch-vllm.sh
@@ -5,7 +5,7 @@
 #SBATCH --reservation=PAWSEY_GPU_COS_TESTING
 #SBATCH --exclusive
 #SBATCH --time=1-00:00:00
-#SBATCH --nodes=2
+#SBATCH --nodes=$1
 #SBATCH --partition=gpu
 module load singularity/4.1.0-slurm
 srun ~/projects/raycluster/start-cluster.py singularity ~/.opt/vLLM/vllm_rocm6.3.1_instinct_vllm0.8.3_20250415.sif \
